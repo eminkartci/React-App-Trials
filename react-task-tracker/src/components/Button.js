be24e4daa5ab9037ad2835730116818fda20b748
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-30 14:01:44 +0300

    Button Clicked Function is implemented

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,14 +1,28 @@
+import PropTypes from 'prop-types'
 
-const Button = (prop) => {
+
+const Button = ({text,color,onClick}) => {
+
     return (
         <div >
-            <button className="btn" >{prop.text}</button>
+            <button 
+            onClick={onClick}
+            className="btn" 
+            style={{background: color}} >
+                {text}
+            </button>
         </div>
     )
 }
 
 Button.defaultProps = {
     text: "Add",
+    color: "light-blue"
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClick: PropTypes.func
 }
 
 export default Button
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
+
+    const onClick = () => {
+        console.log("Clicked Another Method")
+    }
+
     return (
         <header>
             <h1 style={HeadingStyle}>{props.title}</h1>
 
-            <Button />
+            <Button color="red" text= "Click Here !" onClick={onClick}/>
+
         </header>
     )
 }
